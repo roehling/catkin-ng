d6384fc77b6dacb7f46312b3a87e9d11c52b9087
Timo Röhling <timo@gaussglocke.de>
2020-12-27 00:07:40 +0100

    Simplify scan for imported targets

--- hazel/cmake/list_exported_targets.py
+++ hazel/cmake/list_exported_targets.py
@@ -12,9 +12,10 @@ import sys
 import re
 
 targets = set()
-with open(sys.argv[1], "r") as f:
-    for line in f:
-        mo = re.search(r"add_(?:library|executable) *\( *([^ ]+).*IMPORTED", line)
-        if mo:
-            targets.add(mo.group(1))
+for target_file in sys.argv[1:]:
+    with open(target_file, "r") as f:
+        for line in f:
+            mo = re.search(r"add_(?:library|executable) *\( *([^ ]+).*IMPORTED", line)
+            if mo:
+                targets.add(mo.group(1))
 print(";".join(targets))
