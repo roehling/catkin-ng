f44cf9d8e4d0c9615f4aa7a760f396c38ebee4e3
Timo Röhling <timo@gaussglocke.de>
2021-01-13 19:52:17 +0100

    Invoke pip editable install with "."

--- hazel/cmake/python.cmake
+++ hazel/cmake/python.cmake
@@ -94,7 +94,7 @@ function(hazel_python_setup)
             add_custom_command(OUTPUT "${HAZEL_GENERATED_DIR}/python-develspace.stamp"
                 MAIN_DEPENDENCY "${arg_DIRECTORY}/setup.py"
                 WORKING_DIRECTORY "${arg_DIRECTORY}"
-                COMMAND "${CMAKE_COMMAND}" -E env "PYTHONPATH=${PYTHONPATH}" "${HAZEL_PYTHON_EXECUTABLE}" -m pip install --no-deps --prefix "${HAZEL_DEVEL_PREFIX}" "--install-option=--install-dir=${HAZEL_DEVEL_PYTHON_DIR}" "--install-option=--script-dir=${HAZEL_DEVEL_PREFIX}/${script_bindir}" --editable "${arg_DIRECTORY}"
+                COMMAND "${CMAKE_COMMAND}" -E env "PYTHONPATH=${PYTHONPATH}" "${HAZEL_PYTHON_EXECUTABLE}" -m pip install --no-deps --prefix "${HAZEL_DEVEL_PREFIX}" "--install-option=--install-dir=${HAZEL_DEVEL_PYTHON_DIR}" "--install-option=--script-dir=${HAZEL_DEVEL_PREFIX}/${script_bindir}" --editable .
                 COMMAND "${CMAKE_COMMAND}" -E touch "${HAZEL_GENERATED_DIR}/python-develspace.stamp"
                 VERBATIM)
             add_custom_target(python-develspace ALL DEPENDS "${HAZEL_GENERATED_DIR}/python-develspace.stamp")
